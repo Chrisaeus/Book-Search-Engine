1b646ca912db0dd88d7be677fb1fe8562fc2f645
Chrisaeus <87839602+Chrisaeus@users.noreply.github.com>
2022-01-25 22:31:31 -0800

    Made typeDefs more consistent, fleshed out mutation templates in resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,15 +1,29 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-      if (context.user)
-      return User.findOne({ _id: context.user._id });
-    },
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    }
   },
   Mutation: {
+    loginUser: async (parent, { email, password }) => {
 
+    },
+    addUser: async (parent, { username, email, password }) => {
+
+    },
+    saveBook: async (parent, { input }, context) => {
+
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      
+    },
   }
 };
 
