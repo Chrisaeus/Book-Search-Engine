05a05770ab4575cdcd4fa46ef0dcc32b2a65ec65
Chrisaeus <87839602+Chrisaeus@users.noreply.github.com>
2022-01-25 23:53:30 -0800

    removeBook resolver (resolvers complete)

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -45,7 +45,14 @@ const resolvers = {
       throw new AuthenticationError('You need to be logged in!');
     },
     removeBook: async (parent, { bookId }, context) => {
-
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
     },
   }
 };
