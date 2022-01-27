62b41a3ca540bf21b2957a1a837415328933d0c8
Chrisaeus <87839602+Chrisaeus@users.noreply.github.com>
2022-01-26 17:31:56 -0800

    LOGIN_USER mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,7 +1,17 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        bookCount
+        savedBooks
+      }
+    }
+  }
 `;
 
 export const ADD_USER = gql`
